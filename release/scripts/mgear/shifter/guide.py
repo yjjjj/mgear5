007f7b88c07bca410d61d378e59e5ad5c8556d17
miquelcampos <miquel.campos@gmail.com>
2025-01-25 18:12:40 +0900

    change getargspec by getfullargspec #263

--- release/scripts/mgear/shifter/guide.py
+++ release/scripts/mgear/shifter/guide.py
@@ -1464,7 +1464,7 @@ class HelperSlots(object):
 
                 customStep = imp.load_source(fileName, runPath)
                 if hasattr(customStep, "CustomShifterStep"):
-                    argspec = inspect.getargspec(
+                    argspec = inspect.getfullargspec(
                         customStep.CustomShifterStep.__init__
                     )
                     if "stored_dict" in argspec.args:
