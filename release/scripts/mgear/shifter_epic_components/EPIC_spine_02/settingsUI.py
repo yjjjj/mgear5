2e0453ae2c6bc62a1e90965dd828c53b8ff9c154
Miquel Campos <miquel.campos@gmail.com>
2025-06-05 12:43:58 +0900

    [BUG] EPIC_spine_02 : (UI bug) slider "Lock Orient Chest" and spin box aren't synced Fixes #516

--- release/scripts/mgear/shifter_epic_components/EPIC_spine_02/settingsUI.py
+++ release/scripts/mgear/shifter_epic_components/EPIC_spine_02/settingsUI.py
@@ -179,6 +179,8 @@ class Ui_Form(object):
         QtCore.QObject.connect(self.position_spinBox, QtCore.SIGNAL("valueChanged(int)"), self.position_slider.setValue)
         QtCore.QObject.connect(self.lockOri_pelvis_spinBox, QtCore.SIGNAL("valueChanged(int)"), self.lockOri_pelvis_slider.setValue)
         QtCore.QObject.connect(self.lockOri_pelvis_slider, QtCore.SIGNAL("valueChanged(int)"), self.lockOri_pelvis_spinBox.setValue)
+        QtCore.QObject.connect(self.lockOri_chest_spinBox, QtCore.SIGNAL("valueChanged(int)"), self.lockOri_chest_slider.setValue)
+        QtCore.QObject.connect(self.lockOri_chest_slider, QtCore.SIGNAL("valueChanged(int)"), self.lockOri_chest_spinBox.setValue)
         QtCore.QObject.connect(self.softness_slider, QtCore.SIGNAL("valueChanged(int)"), self.softness_spinBox.setValue)
         QtCore.QMetaObject.connectSlotsByName(Form)
 
